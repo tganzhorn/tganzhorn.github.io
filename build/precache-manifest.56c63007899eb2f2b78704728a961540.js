bfee161446d91b0933d9033bd5be03c2a7166647
Tobias Ganzhorn <tganzhorn@gmail.com>
2019-07-02 17:49:54 +0200

    Added Preview of App

---
+++ build/precache-manifest.56c63007899eb2f2b78704728a961540.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d76440074eac05755f284607ee11d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "331940e0298ba21b474f",
+    "url": "/static/css/main.02ac162c.chunk.css"
+  },
+  {
+    "revision": "de0520156ec423915688",
+    "url": "/static/js/2.d16b8d93.chunk.js"
+  },
+  {
+    "revision": "331940e0298ba21b474f",
+    "url": "/static/js/main.c61ed5ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
