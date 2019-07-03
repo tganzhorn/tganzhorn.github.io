4e4bbb4f101b1c737685deb066192e40ce453d4d
Tobias Ganzhorn <tganzhorn@gmail.com>
2019-07-03 12:22:33 +0200

    WIP - Minor Fixes

--- precache-manifest.48ca760dade8b61df8c9f3e0f6cabb36.js
+++ precache-manifest.48ca760dade8b61df8c9f3e0f6cabb36.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d77430eb156a14b4f6faad1c9f27a806",
+    "revision": "0f48084df463a030955ca9e0a682d3fa",
     "url": "/index.html"
   },
   {
-    "revision": "0a51ccfb2df82a8a97b5",
+    "revision": "91ac60840a9ce4f079d9",
     "url": "/static/css/main.7783b39a.chunk.css"
   },
   {
-    "revision": "de0520156ec423915688",
-    "url": "/static/js/2.d16b8d93.chunk.js"
+    "revision": "4cc2aef79b07b0095284",
+    "url": "/static/js/2.163ea8d9.chunk.js"
   },
   {
-    "revision": "0a51ccfb2df82a8a97b5",
-    "url": "/static/js/main.046a75f1.chunk.js"
+    "revision": "91ac60840a9ce4f079d9",
+    "url": "/static/js/main.47645216.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f49225428252142a4de80ae7c942790c.js"
+  "/precache-manifest.48ca760dade8b61df8c9f3e0f6cabb36.js"
 );
 
 self.addEventListener('message', (event) => {
