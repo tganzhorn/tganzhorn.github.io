6aeee6e54519203197759a021bf89eefaeb9fca7
Tobias Ganzhorn <tganzhorn@gmail.com>
2019-07-09 16:32:53 +0200

    Added Inertia App

---
+++ inertia/precache-manifest.e10189d93f18131c6995409f76a6808d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be10a232d1d3d6604739d20b76fa577",
+    "url": "./index.html"
+  },
+  {
+    "revision": "221903c751a0e9bbcc14",
+    "url": "./static/css/main.97c76849.chunk.css"
+  },
+  {
+    "revision": "3ab0b6b6e7c1d89b5631",
+    "url": "./static/js/2.6c237738.chunk.js"
+  },
+  {
+    "revision": "221903c751a0e9bbcc14",
+    "url": "./static/js/main.8b4d833c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
