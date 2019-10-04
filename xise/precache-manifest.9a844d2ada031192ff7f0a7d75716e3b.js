9caef7764f972bc48808e98eb8c4f41fd2fe8cb5
Tobias Ganzhorn <tganzhorn@gmail.com>
2019-10-04 19:57:00 +0200

    dafgdsfgdsfg
    dafgsdfgdsfg

---
+++ xise/precache-manifest.9a844d2ada031192ff7f0a7d75716e3b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d25aba215049a593d3ae6a3bff6c2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4c838d613777185d53a",
+    "url": "/static/js/2.29f0e728.chunk.js"
+  },
+  {
+    "revision": "7ca9a339b335ca452517",
+    "url": "/static/js/main.3c9e1137.chunk.js"
+  },
+  {
+    "revision": "6252884728b6755e2d7e",
+    "url": "/static/js/runtime-main.cd1e9c8e.js"
+  }
+]);
